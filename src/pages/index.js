00f823582706214bde46dfb7306b641f6b3c4ab1
fj <castelloiv@gmail.com>
2020-05-08 14:38:54 -0400

    Adding typhography plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,17 @@
 import React from "react"
+import Styles from "./main.module.scss"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+    <section className={Styles.landing}>
+      <div>
+        {" "}
+        <h1>Blowfish Studio</h1>
+        <p>
+          Custom website development with a focus on eye catching designs and
+          speed.
+        </p>
+      </div>
+    </section>
+  </div>
+)
